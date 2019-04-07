be6231e5ac4ed14629ff618a20d72ddf96a98819
Kiz <kieran.mcdougall@aamcommercial.com.au>
2019-04-08 08:05:36 +1000

    Removed some dead code

--- src/ExternalTask/ExternalTaskService.php
+++ src/ExternalTask/ExternalTaskService.php
@@ -45,13 +45,13 @@ class ExternalTaskService
      *
      * @throws \Exception
      */
-    public function get(string $id): ExternalTask
+    public function get(string $id): FetchExternalTaskTopic
     {
         try {
             $response = $this->client->get(sprintf('/external-task/%s', $id));
 
             return $this->deserializeJson(
-                ExternalTask::class,
+                FetchExternalTaskTopic::class,
                 $response->getBody()->getContents()
             );
         } catch (\GuzzleHttp\Exception\ClientException $clientException) {
@@ -91,68 +91,4 @@ class ExternalTaskService
         }
     }
 
-    /**
-     * Completes a task and updates process variables.
-     *
-     * @param $id
-     * @param FetchAndLockRequest $request
-     *
-     * @throws \Exception
-     */
-    public function handleBPMNError($id, FetchAndLockRequest $request)
-    {
-        $this->client->setRequestUrl(self::EXTERNAL_TASK_URL.$id.'/bpmnError');
-        $this->client->setRequestMethod(self::REQUEST_METHOD_POST);
-        $this->client->setRequestData($request);
-
-        try {
-            $this->client->execute();
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-
-    /**
-     * Completes an external task by id and updates process variables.
-     *
-     * @param $id
-     * @param FetchAndLockRequest $request
-     *
-     * @throws \Exception
-     */
-    public function completeExternalTask($id, FetchAndLockRequest $request)
-    {
-        $this->client->setRequestUrl(self::EXTERNAL_TASK_URL.$id.'/complete');
-        $this->client->setRequestData($request);
-        $this->client->setRequestMethod('POST');
-
-        try {
-            $this->client->execute();
-        } catch (Exception $e) {
-            throw $e;
-        }
-    }
-
-    /**
-     * Reports a failure to execute an external task by id. A number of
-     * retries and a timeout until the task can be retried can be specified.
-     * If retries are set to 0, an incident for this task is created.
-     *
-     * @param $id
-     * @param FetchAndLockRequest $request
-     *
-     * @throws \Exception
-     */
-    public function externalTaskFailed($id, FetchAndLockRequest $request)
-    {
-        $this->client->setRequestUrl(self::EXTERNAL_TASK_URL.$id.'/failure');
-        $this->client->setRequestData($request);
-        $this->client->setRequestMethod('POST');
-
-        try {
-            $this->client->execute();
-        } catch (Exception $e) {
-            throw $e;
-        }
-    }
 }
